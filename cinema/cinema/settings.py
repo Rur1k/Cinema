61db06c5709b91d9854612de7000e16e3b7daafd
Nikita(WorkLaptopLin) <falgerok.master@gmail.com>
2021-07-22 16:44:57 +0300

    настроена работа через gunicorn and nginx

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-07)jyddc)o0*@3fo8i1+31q5g8pt-6$f%r7z4^y1!(be+^f*-@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
